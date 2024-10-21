935b36c3a3c5cb91d2c06ff55bba3bb1f2fee91b
vizibarnabas <vizi.barnabas@gmail.com>
2024-10-21 19:53:09 +0200

    Main edit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from auto import Auto
 autok = [
     Auto("Toyota", "Corolla", 2020, "Benzin", 180),
     Auto("Ford", "Focus", 2018, "Dízel", 190),
-    Auto("BMW", "X5", 2022, "Elektromos", 220),
+    Auto("BMW", "iX5", 2022, "Elektromos", 220),
     Auto("Audi", "A4", 2019, "Benzin", 210)
 ]
 
@@ -12,3 +12,6 @@ for auto in autok:
 
 autok[-1].set_sebesseg(200)
 print(f"\nAz utolsó autó új sebessége: {autok[-1].get_sebesseg()} km/h")
+
+autok[2].set_sebesseg(170)
+print(f"Az {autok[2].get_uzemanyag()} {autok[2].get_modell()}-nek csak {autok[2].get_sebesseg()} a végsebessége.")
